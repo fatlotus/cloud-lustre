a7ff0fba7060ab20736e8350dcda1193540eaf33
Jeremy Archer <open-source@fatlotus.com>
2018-04-01 16:58:51 -0400

    Initial commit of Lustre build automation.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lustre Build Automation for GCP
+
+This repository uses CircleCI workflows to build a new lustre image on GCP.
+
+More documentation forthcoming.
---
+++ make-image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ex
+#
+# Bakes a CentOS 7 lustre image from the latest Lustre RPMs.
+#
+# These instructions were cobbled together from the wiki:
+#   http://wiki.lustre.org/Installing_the_Lustre_Software
+
+ZONE=us-central1-f
+INSTANCE=build-lustre-image
+FAMILY=centos-7-lustre
+
+gcloud compute instances delete -q --zone=$ZONE $INSTANCE || true
+gcloud compute instances create \
+	--image-project=centos-cloud \
+	--image-family=centos-7 \
+	--zone=$ZONE \
+	$INSTANCE
+
+sleep 60
+
+gcloud compute ssh --zone=$ZONE $INSTANCE <make-image-setup.sh
+gcloud compute instances stop $INSTANCE
+
+gcloud compute images create \
+	--source-disk $INSTANCE \
+	--source-disk-zone $ZONE \
+	--family $FAMILY \
+	"${FAMILY}-$(date +%s)"
+
+gcloud compute instances delete -q --zone=$ZONE build-lustre-image
