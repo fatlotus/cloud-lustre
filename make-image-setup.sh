fc90b61102d66f10bb38bb465f89af231d055225
Jeremy Archer <open-source@fatlotus.com>
2018-04-02 22:12:11 -0400

    Install ZFS.

--- make-image-setup.sh
+++ make-image-setup.sh
@@ -44,10 +44,11 @@ sudo yum install -y kmod-lustre \
 	kmod-lustre-osd-ldiskfs \
 	lustre-osd-ldiskfs-mount \
 	lustre \
-	lustre-resource-agents
+	lustre-resource-agents \
+	lustre-osd-zfs
 
-echo lustre | tee /etc/modules-load.d/lustre
-echo ldiskfs | tee -a /etc/modules-load.d/lustre
+echo lustre | sudo tee /etc/modules-load.d/lustre
+echo ldiskfs | sudo tee -a /etc/modules-load.d/lustre
 
 # Shut down the instance so that we can create a clean image.
 sudo shutdown -h +1
